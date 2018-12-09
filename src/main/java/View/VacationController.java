1ffce418409b8dcf2ce6fceee1ae49393c10e8aa
liorpizman <liorpezman@gmail.com>
2018-12-09 11:46:38 +0200

    adding log in to GUI

--- src/main/java/View/VacationController.java
+++ src/main/java/View/VacationController.java
@@ -16,20 +16,43 @@ public class VacationController {
     public javafx.scene.control.TextField FromCityUpdate;
     public javafx.scene.control.TextField ToCountyUpdate;
     public javafx.scene.control.TextField ToCityUpdate;
-    public javafx.scene.control.TextField ArrivalUpdate;
-    public javafx.scene.control.TextField DepartureUpdate;
-    public javafx.scene.control.TextField AccommodationUpdate;
-    public javafx.scene.control.TextField AccommodationRankUpdate;
-    public javafx.scene.control.TextField FlightClassUpdate;
-    public javafx.scene.control.TextField AdultsUpdate;
-    public javafx.scene.control.TextField ChildernUpdate;
-    public javafx.scene.control.TextField BabiesUpdate;
+    public javafx.scene.control.DatePicker ArrivalUpdate;
+    public javafx.scene.control.DatePicker DepartureUpdate;
+    public javafx.scene.control.ChoiceBox AccommodationUpdate;
+    public javafx.scene.control.ChoiceBox AccommodationRankUpdate;
+    public javafx.scene.control.ChoiceBox FlightClassUpdate;
+    public javafx.scene.control.ChoiceBox AdultsUpdate;
+    public javafx.scene.control.ChoiceBox ChildrenUpdate;
+    public javafx.scene.control.ChoiceBox BabiesUpdate;
     public javafx.scene.control.TextField PriceUpdate;
-    public javafx.scene.control.TextField BaggageUpdate;
-    public javafx.scene.control.TextField VacationTypeUpdate;
+    public javafx.scene.control.ChoiceBox BaggageUpdate;
+    public javafx.scene.control.ChoiceBox VacationTypeUpdate;
     public javafx.scene.control.CheckBox TransfersUpdate;
 
     public javafx.scene.control.Button BackButton;
+    public javafx.scene.control.Button showUpdateButton;
+
+    public javafx.scene.control.Button showReadButton;
+    public javafx.scene.control.Button updateButton;
+
+    public javafx.scene.control.TextField VacationIDRead;
+    public javafx.scene.control.TextField FromCountryRead;
+    public javafx.scene.control.TextField FromCityRead;
+    public javafx.scene.control.TextField ToCountyRead;
+    public javafx.scene.control.TextField ToCityRead;
+    public javafx.scene.control.TextField ArrivalRead;
+    public javafx.scene.control.TextField DepartureRead;
+    public javafx.scene.control.TextField AccommodationRead;
+    public javafx.scene.control.TextField AccommodationRankRead;
+    public javafx.scene.control.TextField FlightClassRead;
+    public javafx.scene.control.TextField AdultsRead;
+    public javafx.scene.control.TextField ChildrenRead;
+    public javafx.scene.control.TextField BabiesRead;
+    public javafx.scene.control.TextField PriceRead;
+    public javafx.scene.control.TextField BaggageRead;
+    public javafx.scene.control.TextField VacationTypeRead;
+    public javafx.scene.control.CheckBox TransfersRead;
+
 
     protected static Controller controller;
 
@@ -45,8 +68,7 @@ public class VacationController {
     /**
      * Validation checks for the vacation id
      */
-    public boolean validVacationID() {
-        String id = VacationIDUpdate.getText();
+    public boolean validVacationID(String id) {
         if (id.isEmpty() || !isInteger(id)) {
             Alert a = new Alert(Alert.AlertType.INFORMATION);
             a.setContentText("Invalid Vacation ID, please try again.");
@@ -67,64 +89,67 @@ public class VacationController {
     /**
      * Shows relevant info for vacation id
      */
-    public void showVacation() {
-        if (validVacationID()) {
-            ArrayList<Vacation> OneVacation = searchVacationData(VacationIDUpdate.getText());
-            if(OneVacation.size()<1){
+    public void showVacationRead() {
+        if (validVacationID(VacationIDRead.getText())) {
+            ArrayList<Vacation> OneVacation = searchVacationData(VacationIDRead.getText());
+            if (OneVacation.size() < 1) {
                 Alert a = new Alert(Alert.AlertType.INFORMATION);
                 a.setContentText("Invalid Vacation ID, please try again.");
                 a.show();
-            }
-            else{
+            } else {
                 Vacation currVacation = OneVacation.get(0);
-                FromCountryUpdate.setText(currVacation.getFromOriginFlight().getOriginCountry());
-                FromCountryUpdate.setDisable(false);
+                FromCountryRead.setText(currVacation.getFromOriginFlight().getOriginCountry());
+                FromCountryRead.setDisable(false);
 
-                FromCityUpdate.setText(currVacation.getFromOriginFlight().getOriginCity());
-                FromCityUpdate.setDisable(false);
+                FromCityRead.setText(currVacation.getFromOriginFlight().getOriginCity());
+                FromCityRead.setDisable(false);
 
-                ToCountyUpdate.setText(currVacation.getFromDestFlight().getOriginCountry());
-                ToCountyUpdate.setDisable(false);
+                if (currVacation.getFromDestFlight() != null) {
+                    ToCountyRead.setText(currVacation.getFromDestFlight().getOriginCountry());
+                }
+                ToCountyRead.setDisable(false);
 
-                ToCityUpdate.setText(currVacation.getFromDestFlight().getOriginCity());
-                ToCityUpdate.setDisable(false);
+                if (currVacation.getFromDestFlight() != null) {
+                    ToCityRead.setText(currVacation.getFromDestFlight().getOriginCity());
+                }
+                ToCityRead.setDisable(false);
 
-                ArrivalUpdate.setText(currVacation.getStartDate());
-                ArrivalUpdate.setDisable(false);
+                ArrivalRead.setText(currVacation.getStartDate());
+                ArrivalRead.setDisable(false);
 
-                DepartureUpdate.setText(currVacation.getEndDate());
-                DepartureUpdate.setDisable(false);
+                DepartureRead.setText(currVacation.getEndDate());
+                DepartureRead.setDisable(false);
 
-                AccommodationUpdate.setText(currVacation.getAccommodationType());
-                AccommodationUpdate.setDisable(false);
+                AccommodationRead.setText(currVacation.getAccommodationType());
+                AccommodationRead.setDisable(false);
 
-                AccommodationRankUpdate.setText(Integer.toString(currVacation.getAccommodationRank()));
-                AccommodationRankUpdate.setDisable(false);
+                AccommodationRankRead.setText(Integer.toString(currVacation.getAccommodationRank()));
+                AccommodationRankRead.setDisable(false);
 
-                FlightClassUpdate.setText(currVacation.getFromOriginFlight().getTicketType());
-                FlightClassUpdate.setDisable(false);
+                FlightClassRead.setText(currVacation.getFromOriginFlight().getTicketType());
+                FlightClassRead.setDisable(false);
 
-                AdultsUpdate.setText(Integer.toString(currVacation.getFromOriginFlight().getAdultTicketsAmount()));
-                AdultsUpdate.setDisable(false);
+                AdultsRead.setText(Integer.toString(currVacation.getFromOriginFlight().getAdultTicketsAmount()));
+                AdultsRead.setDisable(false);
 
-                ChildernUpdate.setText(Integer.toString(currVacation.getFromOriginFlight().getChildTicketsAmount()));
-                ChildernUpdate.setDisable(false);
+                ChildrenRead.setText(Integer.toString(currVacation.getFromOriginFlight().getChildTicketsAmount()));
+                ChildrenRead.setDisable(false);
 
-                BabiesUpdate.setText(Integer.toString(currVacation.getFromOriginFlight().getBabyTicketsAmount()));
-                BabiesUpdate.setDisable(false);
+                BabiesRead.setText(Integer.toString(currVacation.getFromOriginFlight().getBabyTicketsAmount()));
+                BabiesRead.setDisable(false);
 
-                PriceUpdate.setText(Double.toString(currVacation.getPrice()));
-                PriceUpdate.setDisable(false);
+                PriceRead.setText(Double.toString(currVacation.getPrice()));
+                PriceRead.setDisable(false);
 
-                BaggageUpdate.setText(currVacation.getBaggageType());
-                BaggageUpdate.setDisable(false);
+                BaggageRead.setText(currVacation.getBaggageType());
+                BaggageRead.setDisable(false);
 
-                VacationTypeUpdate.setText(currVacation.getVacationType());
-                VacationTypeUpdate.setDisable(false);
+                VacationTypeRead.setText(currVacation.getVacationType());
+                VacationTypeRead.setDisable(false);
 
 
-                TransfersUpdate.setSelected(currVacation.isTransfers());
-                TransfersUpdate.setDisable(false);
+                TransfersRead.setSelected(currVacation.isTransfers());
+                TransfersRead.setDisable(false);
             }
         }
     }
@@ -147,5 +172,83 @@ public class VacationController {
             return false;
         }
         return true;
+    }
+
+
+    /**
+     * Shows relevant info for vacation id for update
+     */
+    public void showVacationUpdate() {
+        if (validVacationID(VacationIDUpdate.getText())) {
+            ArrayList<Vacation> OneVacation = searchVacationData(VacationIDUpdate.getText());
+            if (OneVacation.size() < 1) {
+                Alert a = new Alert(Alert.AlertType.INFORMATION);
+                a.setContentText("Invalid Vacation ID, please try again.");
+                a.show();
+            } else {
+                Vacation currVacation = OneVacation.get(0);
+                FromCountryUpdate.setText(currVacation.getFromOriginFlight().getOriginCountry());
+                FromCountryUpdate.setDisable(false);
+
+                FromCityUpdate.setText(currVacation.getFromOriginFlight().getOriginCity());
+                FromCityUpdate.setDisable(false);
+
+                if (currVacation.getFromDestFlight() != null) {
+                    ToCountyUpdate.setText(currVacation.getFromDestFlight().getOriginCountry());
+                }
+                ToCountyUpdate.setDisable(false);
+
+                if (currVacation.getFromDestFlight() != null) {
+                    ToCityUpdate.setText(currVacation.getFromDestFlight().getOriginCity());
+                }
+                ToCityUpdate.setDisable(false);
+
+                String[] StartDate = currVacation.getStartDate().split("-");
+                LocalDate Sdate = LocalDate.of(Integer.parseInt(StartDate[0]), Integer.parseInt(StartDate[1]), Integer.parseInt(StartDate[2]));
+                ArrivalUpdate.setValue(Sdate);
+                ArrivalUpdate.setDisable(false);
+
+                String[] EndDate = currVacation.getEndDate().split("-");
+                LocalDate Edate = LocalDate.of(Integer.parseInt(EndDate[0]), Integer.parseInt(EndDate[1]), Integer.parseInt(EndDate[2]));
+                DepartureUpdate.setValue(Edate);
+                DepartureUpdate.setDisable(false);
+
+                AccommodationUpdate.setValue(currVacation.getAccommodationType());
+                AccommodationUpdate.setDisable(false);
+
+                AccommodationRankUpdate.setValue(Integer.toString(currVacation.getAccommodationRank()));
+                AccommodationRankUpdate.setDisable(false);
+
+                FlightClassUpdate.setValue(currVacation.getFromOriginFlight().getTicketType());
+                FlightClassUpdate.setDisable(false);
+
+                AdultsUpdate.setValue(Integer.toString(currVacation.getFromOriginFlight().getAdultTicketsAmount()));
+                AdultsUpdate.setDisable(false);
+
+                ChildrenUpdate.setValue(Integer.toString(currVacation.getFromOriginFlight().getChildTicketsAmount()));
+                ChildrenUpdate.setDisable(false);
+
+                BabiesUpdate.setValue(Integer.toString(currVacation.getFromOriginFlight().getBabyTicketsAmount()));
+                BabiesUpdate.setDisable(false);
+
+                PriceUpdate.setText(Double.toString(currVacation.getPrice()));
+                PriceUpdate.setDisable(false);
+
+                BaggageUpdate.setValue(currVacation.getBaggageType());
+                BaggageUpdate.setDisable(false);
+
+                VacationTypeUpdate.setValue(currVacation.getVacationType());
+                VacationTypeUpdate.setDisable(false);
+
+
+                TransfersUpdate.setSelected(currVacation.isTransfers());
+                TransfersUpdate.setDisable(false);
+            }
+        }
+    }
+
+
+    public void updateVacation(){
+
     }
 }
--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -185,7 +185,7 @@ public class View {
         try {
             Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("UpdateVacationWindow.fxml"));
             root.getStylesheets().add(getClass().getClassLoader().getResource("flightCSS.css").toExternalForm());
-            Scene scene = new Scene(root, 600, 500);
+            Scene scene = new Scene(root, 600, 600);
             stage.setScene(scene);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.show();
